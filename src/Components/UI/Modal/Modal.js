1ed126972039f706ffda813e2c2737fc4d737cb6
“alka-si” <alkasimon23@gmail.com>
2020-10-07 21:29:22 +0530

    Optimized update for Modal, OrderSummary comp

--- src/Components/UI/Modal/Modal.js
+++ src/Components/UI/Modal/Modal.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../Hoc/Aux'
 import classes from './Modal.css'
 import Backdrop from '../Backdrop/Backdrop'
 
-const Modal = (props) => {
-    return(
-        <Aux>
-            <Backdrop show={props.show} dismiss={props.modalClosed}/>
-            <div 
-                className={classes.Modal}
-                style={{transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                        opacity: props.show ? '1' : '0'}}>
-                {props.children}
-            </div>
-        </Aux>
-    )
+class Modal extends Component{
+
+        componentWillUpdate(){
+            console.log("[Modal] WillUpdate")
+        }
+
+        //This is to optimize as the modal should only be updates when ordersummary shown.
+        shouldComponentUpdate(nextProps, nextState){
+            return nextProps.show !== this.props.show
+        }
+
+        render(){
+            return(
+                <Aux>
+                    <Backdrop show={this.props.show} dismiss={this.props.modalClosed}/>
+                    <div 
+                        className={classes.Modal}
+                        style={{transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                                opacity: this.props.show ? '1' : '0'}}>
+                        {this.props.children}
+                    </div>
+                </Aux>
+            )
+    }
 };
 
 export default Modal;
